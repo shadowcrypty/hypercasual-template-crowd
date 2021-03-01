4e6cc37e4e3de2d32af1c3473963116bcdcbfb0c
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-03-01 18:25:13 +0300

    created:crowd stack

---
+++ Assets/Scripts/AddCrowd.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Crowd.Member
+{
+
+
+    public class AddCrowd : MonoBehaviour
+    {
+        public GameObject _member;
+        public float _aci ;
+        public float _offset;
+
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag=="Player" && this.tag != "Player")
+            {
+                transform.SetParent(_member.transform);
+                _aci = _member.transform.childCount - 1;
+                _aci *= 90;
+                transform.position=new Vector3();
+                this.tag = "Player";
+            }
+        }
+        
+
+    }
+}

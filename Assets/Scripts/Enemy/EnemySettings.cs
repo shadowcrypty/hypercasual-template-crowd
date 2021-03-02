b9adb2891bb8f4ee678931f03b0999bd61e22c53
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-03-02 13:44:12 +0300

    created:enemy ai created: navmesh map , navmesh agent

---
+++ Assets/Scripts/Enemy/EnemySettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Crowd.Enemy
+{
+
+    [CreateAssetMenu(menuName = "Crowd/Enemy/EnemySettings")]
+    public class EnemySettings : ScriptableObject
+    {
+        [SerializeField] private float _lookRadius=1;
+        public float LookRadius { get { return _lookRadius; } }
+        
+    }
+}
---
+++ Assets/Scripts/Enemy/Enemycontroller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Crowd.Enemy
+{
+    public class Enemycontroller : MonoBehaviour
+    {
+        [SerializeField] private EnemySettings _enemySettings;
+        NavMeshAgent _agent;
+        GameObject[] _crowds;
+        
+
+        private void Start()
+        {
+            _crowds = GameObject.FindGameObjectsWithTag("Crowd");
+
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _enemySettings.LookRadius);
+
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < _crowds.Length; i++)
+            {
+                float distance = Vector3.Distance(transform.position,_crowds[i].transform.position);
+                if (distance <= _enemySettings.LookRadius)
+                {
+                    _agent.SetDestination(_crowds[i].transform.position);
+                    //break;
+                    
+                    //_agent.nextPosition = _crowds[i].transform.position;
+
+                }
+
+
+            }
+            
+        }
+    }
+}

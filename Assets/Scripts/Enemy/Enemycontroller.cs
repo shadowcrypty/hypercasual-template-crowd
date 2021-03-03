f68f2b742f0d93ecff98694545b36ad37975c5e2
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-03-03 18:57:06 +0300

    created: ienumarator method

--- Assets/Scripts/Enemy/Enemycontroller.cs
+++ Assets/Scripts/Enemy/Enemycontroller.cs
@@ -7,6 +7,7 @@ namespace Crowd.Enemy
 {
     public class Enemycontroller : MonoBehaviour
     {
+        //List<T> ts = new List<T>;
         [SerializeField] private EnemySettings _enemySettings;
         NavMeshAgent _agent;
         GameObject[] _crowds;
@@ -14,9 +15,11 @@ namespace Crowd.Enemy
 
         private void Start()
         {
+            //ts.Sort()
             _crowds = GameObject.FindGameObjectsWithTag("Crowd");
 
             _agent = GetComponent<NavMeshAgent>();
+            StartCoroutine(chase());
         }
 
 
@@ -27,23 +30,37 @@ namespace Crowd.Enemy
 
         }
 
-        private void Update()
+        public IEnumerator chase()
         {
             for (int i = 0; i < _crowds.Length; i++)
             {
-                float distance = Vector3.Distance(transform.position,_crowds[i].transform.position);
+                Debug.Log("Start");
+                _crowds = GameObject.FindGameObjectsWithTag("Crowd");
+                float distance = Vector3.Distance(transform.position, _crowds[i].transform.position);
                 if (distance <= _enemySettings.LookRadius)
                 {
                     _agent.SetDestination(_crowds[i].transform.position);
-                    //break;
+                    Debug.Log(_crowds[i]);
                     
-                    //_agent.nextPosition = _crowds[i].transform.position;
 
+                    break;
+                    //yield return null;
                 }
-
+                //
 
             }
-            
+            yield return new WaitForSeconds(3f);
+
+            Debug.Log("restarted");
+            StartCoroutine(chase());
+           
+
+
+        }
+        private void Update()
+        {
+           
+
         }
     }
 }
